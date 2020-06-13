fd91a749583a6658699e4cecffe1a3131f4fec18
Kirill Shevtsov <kirwork.em@gmail.com>
2020-06-13 21:31:43 +0300

    Adding views imports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './views/view-components/form-fields/firstFormFields';
+import './views/view-components/form-fields/secondFormFields';
+import './views/view-components/form-fields/thirdFormFields';
 import './styles/index.scss';

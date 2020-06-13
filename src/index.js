ea0ccfc34b0036fe44795b9538b95aa142806c77
Kirill Shevtsov <kirwork.em@gmail.com>
2020-06-14 02:33:29 +0300

    Adding button handlers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './views/view-components/form-fields/firstFormFields';
 import './views/view-components/form-fields/secondFormFields';
 import './views/view-components/form-fields/thirdFormFields';
+
+import './controllers/interface-interactions/buttonHandlers';
+
 import './styles/index.scss';

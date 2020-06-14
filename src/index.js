11baa57992d47a6cd4a449cd0d73484a0da1604c
Kirill Shevtsov <kirwork.em@gmail.com>
2020-06-15 00:59:47 +0300

    Adding form handlers import

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import './views/view-components/form-fields/secondFormFields';
 import './views/view-components/form-fields/thirdFormFields';
 
 import './controllers/interface-interactions/buttonHandlers';
+import './controllers/interface-interactions/inputHandlers';
+import './controllers/interface-interactions/formHandlers';
 
 import './styles/index.scss';
